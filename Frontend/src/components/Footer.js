cc51e832f45f7aebf63e030b16ba71282a82ef57
Sameet <sameetghouri@yahoo.com>
2023-02-09 00:47:20 +0500

    modified

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -16,10 +16,10 @@ const Footer = () => {
         <div className="hidden  lg:block lg:w-1/4 lg:fixed lg:right-0  lg:h-screen bg-gre">
         
         <nav className=" flex flex-col items-center justify-center pt-4 ">
-            {!user && <div className='w-3/5 bg-white shadow-md text-center font-semibold text-gray-900 my-2 px-2 py-4 hover:bg-bre hover:text-white rounded-lg '><Link  className=" py-4 pointer-cursor" to='/signup'>Sign up</Link></div>}
-            {!user && <div className='w-3/5 bg-white shadow-md text-center font-semibold text-gray-900 my-2 px-2 py-4 hover:bg-bre hover:text-white rounded-lg '><Link  className=" py-4 pointer-cursor" to='/signin'>Log in</Link></div>}
-            {user && <div className='w-3/5 bg-white shadow-md text-center font-semibold text-gray-900  my-2 px-2 py-4 hover:bg-bre hover:text-white  rounded-lg '><Link  className=" py-4 pointer-cursor" to='/create'>Create Post</Link></div>}
-            {user && <div className='w-3/5 bg-white shadow-md text-center font-semibold text-gray-900  my-2  hover:bg-bre hover:text-white  rounded-lg '>
+            {!user && <div className='w-3/5 bg-gray-800 shadow-md text-center font-semibold   my-2 px-2 py-4 hover:bg-bre text-white rounded-lg '><Link  className=" py-4 pointer-cursor" to='/signup'>Sign up</Link></div>}
+            {!user && <div className='w-3/5 bg-gray-800 shadow-md text-center font-semibold   my-2 px-2 py-4 hover:bg-bre text-white rounded-lg '><Link  className=" py-4 pointer-cursor" to='/signin'>Log in</Link></div>}
+            {user && <div className='w-3/5 bg-gray-800 shadow-md text-center font-semibold text-g  my-2 px-2 py-4 hover:bg-bre text-white  rounded-lg '><Link  className=" py-4 pointer-cursor" to='/create'>Create Post</Link></div>}
+            {user && <div className='w-3/5 bg-gray-800 shadow-md text-center font-semibold   my-2  hover:bg-bre text-white  rounded-lg '>
             <button  className=" py-4 px-2 pointer-cursor" onClick={handleClick}>Log Out</button> 
             </div>}
           
--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -17,12 +17,12 @@ const Navbar = () => {
         
             <div className="  hidden sm:bg-gre sm:w-1/4  sm:h-screen sm:flex sm:flex-col sm:items-center sm:pt-3 sm:fixed  sm:overflow-hidden">
             <div className='w-full text-center mb-2 font-bold  rounded px-2 py-4 text-4xl text-bre '><Link  to='/'>OKPK</Link></div>
-            {user && <div className='w-3/5 bg-white  text-center my-2  hover:bg-bre hover:text-white rounded-xl  px-2 py-4 shadow-md  hover:shadow-lg '><Link className="  py-4 pointer-cursor" to='/'>NewsFeed</Link></div>}
-            {user && <div className='w-3/5 bg-white  text-center my-2  hover:bg-bre hover:text-white  rounded-xl  px-2 py-4 shadow-md  hover:shadow-lg '><Link className="  py-4 pointer-cursor" to='/profile'>Profile</Link></div>}            
-            {!user && <div className='lg:hidden w-3/5 bg-white text-center my-2  hover:bg-bre hover:text-white  rounded-xl  px-2 py-4 shadow-md  hover:shadow-lg '><Link className=" py-4 pointer-cursor" to='/signup'>Sign up</Link></div>}
-            {!user && <div className='lg:hidden w-3/5 bg-white text-center my-2  hover:bg-bre hover:text-white  rounded-xl  px-2 py-4 shadow-md  hover:shadow-lg '><Link className=" py-4 pointer-cursor" to='/signin'>Log in</Link></div>}
-            {user && <div className='lg:hidden w-3/5 bg-white text-center my-2  hover:bg-bre hover:text-white  rounded-xl  px-2 py-4 shadow-md  hover:shadow-lg '><Link className=" py-4  pointer-cursor" to='/create'>Create Post</Link></div>}
-            {user && <div className='lg:hidden w-3/5 bg-white text-center my-2  hover:bg-bre hover:text-white  rounded-xl   shadow-md  hover:shadow-lg '>
+            {user && <div className='w-4/6  text-center my-2    rounded-lg  px-2 py-4 bg-white shadow-sm'><Link className="  py-4 pointer-cursor font-bold hover:text-bre" to='/'>NewsFeed</Link></div>}
+            {user && <div className='w-4/6  text-center my-2   rounded-lg  px-2 py-4 bg-white shadow-sm  '><Link className="  py-4 pointer-cursor font-bold hover:text-bre" to='/profile'>Profile</Link></div>}            
+            {!user && <div className='lg:hidden w-4/6 bg-white text-center my-2  rounded-lg  px-2 py-4 shadow-sm  '><Link className=" py-4 pointer-cursor font-bold hover:text-bre" to='/signup'>Sign up</Link></div>}
+            {!user && <div className='lg:hidden w-4/6 bg-white text-center my-2  rounded-lg  px-2 py-4 shadow-sm  '><Link className=" py-4 pointer-cursor" to='/signin'>Log in</Link></div>}
+            {user && <div className='lg:hidden w-4/6 bg-white text-center my-2  rounded-lg  px-2 py-4 shadow-sm  '><Link className=" py-4  pointer-cursor" to='/create'>Create Post</Link></div>}
+            {user && <div className='lg:hidden w-4/6 bg-white text-center my-2  rounded-lg   shadow-sm  '>
             <button className=" py-4 px-2  pointer-cursor " onClick={handleClick}>Log Out</button> </div>}
            
             </div>
