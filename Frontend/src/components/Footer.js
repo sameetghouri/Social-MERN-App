c05cc04ecbce5aad29efb25e898b01bb11bad7f2
Sameet <sameetghouri@yahoo.com>
2023-03-01 14:49:33 +0500

    modified

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -16,11 +16,7 @@ const Footer = () => {
         <div className="hidden  lg:block lg:w-1/4 lg:fixed lg:right-0  lg:h-screen bg-gre">
         
         <nav className=" flex flex-col items-center justify-center pt-4 ">
-            {!user && <Link  className="w-3/5 bg-white shadow-sm text-center   my-2 px-2 py-4  rounded-lg pointer-cursor font-bold hover:text-bre" to='/signup'>Sign up</Link>}
-            {!user && <Link  className="w-3/5 bg-white shadow-sm text-center my-2 px-2 py-4 rounded-lg pointer-cursor font-bold hover:text-bre" to='/signin'>Log in</Link>}
-            {user && <Link  className="w-3/5 bg-white shadow-sm text-center text-g  my-2 px-2 py-4 rounded-lg pointer-cursor font-bold hover:text-bre " to='/create'>Create Post</Link>}
-            {user && <button  className="w-3/5 bg-white shadow-sm text-center my-2 rounded-lg py-4 px-2 pointer-cursor font-bold hover:text-bre" onClick={handleClick}>Log Out</button>}
-        </nav>
+           </nav>
     </div>
      );
 }
